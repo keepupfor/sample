e8e8d12491d5c425033b6178bd1ec5042e3b67d8
keepupfor <namexd@163.com>
2017-08-25 13:13:11 +0800

    Finish login-logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,29 +9,30 @@ class UsersController extends Controller
 {
     //
     public function create()
-     {
+    {
 
-         return view('users/create');
-     }
+        return view('users/create');
+    }
 
-     public function show(User $user)
-     {
-         return view('users.show',compact('user'));
-     }
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
 
-     public function store(Request $request)
-     {
-         $this->validate($request,[
-             'name'=>'required|max:50',
-             'email'=>'required|email|unique:users|max:255',
-             'password'=>'required'
-         ]);
-         $user=User::create([
-             'name'=>$request->name,
-             'email'=>$request->email,
-             'password'=>bcrypt($request->password),
-         ]);
-         session()->flash('success','欢迎来到实力至上主义的教室');
-         return redirect()->route('users.show',[$user]);
-     }
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required'
+        ]);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        Auth::login($user);
+        session()->flash('success', '欢迎来到实力至上主义的教室');
+        return redirect()->route('users.show', [$user]);
+    }
 }
