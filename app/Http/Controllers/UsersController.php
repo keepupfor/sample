3c2d81548c182519026e712b0332cb15182d81e8
keepupfor <namexd@163.com>
2017-08-25 16:47:51 +0800

    Finish user edit,update ,index,and destroy actions

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,9 +8,24 @@ use App\Models\User;
 class UsersController extends Controller
 {
     //
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'except' => ['create', 'show', 'store', 'index']
+        ]);
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
+    }
+
     public function create()
     {
-
         return view('users/create');
     }
 
@@ -32,7 +47,39 @@ class UsersController extends Controller
             'password' => bcrypt($request->password),
         ]);
         Auth::login($user);
-        session()->flash('success', '欢迎来到实力至上主义的教室');
+        session()->flash('success', '欢迎您的到来');
         return redirect()->route('users.show', [$user]);
+    }
+
+    public function edit(User $user)
+    {
+        $this->authorize('update_edit', $user);
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+        $this->authorize('update_edit', $user);
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+        session()->flash('success', '修改个人信息成功');
+        return redirect()->route('users.show', $user->id);
+    }
+
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '删除用户成功');
+        return back();
     }
 }
