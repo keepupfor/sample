5e406cb0ac726800c4c03f9bde8b4a57ffd2f4d3
keepupfor <namexd@163.com>
2017-08-29 11:20:42 +0800

    Add user follow

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -4,6 +4,7 @@
   @if (Auth::check())
     <div class="row">
       <div class="col-md-8">
+
         <section class="status_form">
           @include('shared._status_form')
         </section>
@@ -13,6 +14,9 @@
       <aside class="col-md-4">
         <section class="user_info">
           @include('shared._user_info', ['user' => Auth::user()])
+        </section>
+        <section class="stats">
+          @include('shared._stats', ['user' => Auth::user()])
         </section>
       </aside>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@
 */
 
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
@@ -33,3 +35,7 @@ Route::group(['namespace'=>'Auth','prefix'=>'password'],function (){
 });
 
 Route::resource('statuses', 'StatusesController',['only'=>['store','destroy']]);
+Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
+Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
+Route::post('/users/followers/{user}', 'FollowersController@store')->name('followers.store');
+Route::delete('/users/followers/{user}', 'FollowersController@destroy')->name('followers.destroy');
